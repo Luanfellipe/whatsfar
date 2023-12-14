e1211c68382a7b91633af9badc5bb34d890d9495
Luanfellipe <luanfellipefoto@hotmail.com>
2023-12-14 17:40:03 -0300

    Update code.js

--- code.js
+++ code.js
@@ -9,3 +9,26 @@
         "left": "-100%"
       });
     });
+
+$(".heading-compose").click(function() {
+    // Antes de abrir a barra lateral, faça uma chamada à API do Bitrix24 para obter informações
+    getBitrixInfoAndUpdateUI();
+
+    // Abra a barra lateral
+    $(".side-two").css({
+        "left": "0"
+    });
+});
+
+$(".newMessage-back").click(function() {
+    // Feche a barra lateral
+    $(".side-two").css({
+        "left": "-100%"
+    });
+});
+
+// Função para obter informações do Bitrix24 e atualizar a interface do usuário
+function getBitrixInfoAndUpdateUI() {
+    // Adapte esta função conforme necessário para fazer chamadas à API do Bitrix24
+    // e atualizar a interface do usuário com as informações obtidas.
+}
